2f8058675bed1b0a3e75ad85f119d50385a32b10
Rajesh  Shenoy <rshenoy@bp-3.com>
2024-02-02 11:45:45 +0000

    first commit

--- initial/src/main/java/com/example/producingwebservice/ProducingWebServiceApplication.java
+++ initial/src/main/java/com/example/producingwebservice/ProducingWebServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProducingWebServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProducingWebServiceApplication.class, args);
 	}
